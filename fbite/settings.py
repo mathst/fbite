d2514c24d9a27eb78deae67a530497c0220ce411
mathst <matheusemox@gmail.com>
2023-06-25 22:55:14 -0300

    aa

--- fbite/settings.py
+++ fbite/settings.py
@@ -193,7 +193,10 @@ SOCIAL_AUTH_URL_NAMESPACE = 'social'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = STATIC_URL = '/static/'
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
